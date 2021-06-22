e820117b831922ff99b26aaa5ca3b41dd8538eff
Anna <aaraszewska@gmail.com>
2021-06-22 15:22:11 +0100

    Add project files.

---
+++ Navsta/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Navsta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
