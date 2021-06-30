1faa3e3b22ae275b074c8052182f64378a49d6ec
lee-cordell <41643283+lee-cordell@users.noreply.github.com>
2021-06-30 16:44:59 +0100

    Create ABDRobotData.cs

---
+++ NavstaBLL/ABDRobotData.cs
@@ -0,0 +1,3 @@
+///this class is responsible for holding the data structure of the abd data
+
+///initially we want properties for the standard channels. Later we will handle channels that are not known until runtime
