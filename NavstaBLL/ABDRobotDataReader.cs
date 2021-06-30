d6a5294b4c7c165225a122fea2f6fe94f141f0b3
lee-cordell <41643283+lee-cordell@users.noreply.github.com>
2021-06-30 16:43:03 +0100

    Create ABDRobotDataReader.cs

---
+++ NavstaBLL/ABDRobotDataReader.cs
@@ -0,0 +1,18 @@
+///<summary>Responsible for reading data from a abd robot</summary>
+
+///add a method for getting a channel ordinal position for a channel name
+
+///add a method for reading entire robot file into memory
+
+/*we're very interested in the standard channels: 
+satellites
+time
+latitude
+longitude
+velocity kmh
+heading
+height
+
+...so assume that you will have properties on a data model for these fields. The other channels can just be an array or list of channels. We can do this bit later - let's focus on the standard channels first.
+
+*/
