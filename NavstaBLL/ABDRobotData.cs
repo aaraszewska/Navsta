e43b15f177f8bc62e4fd01ae41f4571627467d9f
Anna <aaraszewska@gmail.com>
2021-07-08 15:05:06 +0100

    vbo2

--- NavstaBLL/ABDRobotData.cs
+++ NavstaBLL/ABDRobotData.cs
@@ -12,79 +12,53 @@ namespace NavstaBLL
     /// </summary>
     public class ABDRobotData
     {
-        #region Private fields
-        private int _satallites;
-        private float _time;
-        private float _longitude;
-        private float _latitude;
-        private float _velocity;
-        private float _heading;
-        private float _height;
+        
+       #region Properties
+        public int Satellites { get; set; }
+        public DateTime Time { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public float Velocity { get; set; }
+        public double Heading { get; set; }
+        public double Height { get; set; }
         #endregion
 
-        #region Properties
-        public int Saltellites
-        {
-            get { return _satallites; }
-            set { _satallites = value; }
-        }
+    }
+    //tuple dynamic list of chanels data 2 independent list
+    //list of tuple
+    public List<(string Chanelname, float chanelValue)> AdditionalChanels { get; set; } = new List<(string chanelname, float chanelValue)>();
+    public List<string>ChanelName { get; set; } = new List<string>();
+    public List<float> ChanelData { get; set; } = new List<float>();
 
-
-        public float Time
-        {
-            get { return _time; }
-            set
-            {
-                _time = value;
-
-                //TimeSpan time = TimeSpan.FromSeconds(Convert.ToDouble(_time));
-                //var str  = time.ToString(@"hh\:mm\:ss");
-
-
-            }
-        }
-
-
-        public float Latitude
-        {
-            get { return _latitude; }
-            set { _latitude = value; }
-        }
-
-
-        public float Longitude
-        {
-            get { return _longitude; }
-            set { _longitude = value; }
-        }
-
-
-        public float Velocity
-        {
-            get { return _velocity; }
-            set { _velocity = value; }
-        }
-
-
-        public float Heading
-        {
-            get { return _heading; }
-            set { _heading = value; }
-        }
-
-
-        public float Height
-        {
-            get { return _height; }
-            set { _height = value; }
-        }
-
-        #endregion
-
-        List<string> listOfChanels = new List<string>();
-
-    } 
+    public class Adddata
+    {
+       public string ChanelName { get; set; }
+       public float ChanelData { get; set; }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
 
 
 
--- NavstaBLL/ABDRobotDataReader.cs
+++ NavstaBLL/ABDRobotDataReader.cs
@@ -32,13 +32,35 @@ namespace NavstaBLL
     {
         #region ReadData
         /// <summary>
-        ///  method for reading entire robot file into memory
+        ///  method for reading entire robot file into memory abdatarobot collection robot data
+        ///  //must return somethink.ma zwrocic list of sat,tim,lat,lon,vel,hea,hei
         /// </summary>
-        public static void ReadRobotFile()
+        public  void ReadRobotFile()
         {
 
             string filePath = @"C:\Users\Anna\Desktop\Test3.txt";
             List<string> lines = File.ReadAllLines(filePath).ToList();
+            var robotData = new List<ABDRobotData>();
+            //for each line you need to create a abdrobotdata model
+            lines.ForEach(l => robotData.Add(new ABDRobotData() //specjalna akcja dla listy
+            {
+                Satellites = l.Split("\t")   //need split the lines columns by tab split kolumn dla kazdej lini
+                Time = l.Split("\t")
+                Latitude = l.Split("\t")
+                Longitude = l.Split("\t")
+                Velocity = l.Split("\t")
+                Heading = l.Split("\t")
+                Height = l.Split("\t")
+            })); 
+
+            // witch column is mptime
+            //for each line you need to create a abdrobotdata column name for chanel whitch we looking 
+            //
+
+
+
+
+            
 
 
         }
@@ -50,7 +72,7 @@ namespace NavstaBLL
         /// method for getting a channel ordinal position for a channel name
         /// </summary>
 
-        public static void ReadChanelPosition()
+        public  void ReadChanelPosition()
         {
             string filePath = @"C:\Users\Anna\Desktop\Test3.txt";
             
